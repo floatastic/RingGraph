1dfd6c3c464daaa9dbda3d201072f300bc978e45
MichalKreft <kreft.michal@gmail.com>
2015-04-13 23:47:21 +0200

    Initial Commit

---
+++ RingGraph/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RingGraph
+//
+//  Created by Michał Kreft on 13/04/15.
+//  Copyright (c) 2015 Michał Kreft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
